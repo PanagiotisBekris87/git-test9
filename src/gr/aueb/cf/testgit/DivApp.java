1344abcb2514985a7cfb9763e5652057ef08c078
Panagiotis Bekris <pbekris8@gmail.com>
2025-12-03 17:15:17 +0200

    Fix: div App

--- src/gr/aueb/cf/testgit/DivApp.java
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -1,13 +1,26 @@
 package gr.aueb.cf.testgit;
 
+import java.util.Scanner;
+
 public class DivApp {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
+
+        do {
+            System.out.println("Please insert nominator and denominator.");
+            System.out.println("Denominator cannot be zero.");
+            num1 = scanner.nextInt();
+            num2 = scanner.nextInt();
+        } while (num2 != 0);
+
+        System.out.printf("Division result of %d and %d is: %d\n", num1, num2, divide(num1, num2));
     }
 
     public static int divide(int a, int b) {
-        if (b == 0) return 0;
-        else return a/b;
+        return a / b;
     }
 }
