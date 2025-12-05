7812acff95c9356a74efc014ae1a60c406c0567c
Panagiotis Bekris <pbekris8@gmail.com>
2025-12-05 17:17:00 +0200

    feat: add mulapp class

--- src/gr/aueb/cf/testgit/MulApp.java
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -3,7 +3,13 @@ package gr.aueb.cf.testgit;
 public class MulApp {
 
     static void main() {
+        int a = 8;
+        int b = 15;
+        int result = 0;
 
+        result = multiply(a, b);
+
+        System.out.println("result = " + result);
     }
 
     public static int multiply(int a, int b) {
